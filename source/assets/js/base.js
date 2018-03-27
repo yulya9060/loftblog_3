f165889eb0c6935b44c63856f63df9f8520e70dd
Yuliya Saltrukovich <yulya_9060@mail.ru>
2018-03-27 22:25:33 +0300

    адаптив

--- source/assets/js/base.js
+++ source/assets/js/base.js
@@ -3,6 +3,7 @@ import '../../components/hamburger/hamburger.js';
 import './mapbasics.js';
 import './picturefill.js';
 import '../../components/fixedNav/fixedNav.js';
+import '../../components/header/header.js';
 import svg4everybody from 'svg4everybody';
 
 svg4everybody();
